1cd6b59600300da32cf302ab175b582f8ed6d309
youngwooJang <duddn5906@naver.com>
2021-05-29 23:30:14 +0900

    theory finished

---
+++ Vanila Java/Theory/index.js
@@ -0,0 +1,13 @@
+const nicoInfo = {
+    name:"Nico",
+    age:33,
+    gender:"Male",
+    isHandsome:true,
+    favMovies:["Along the gods","Thanks very much"],
+    favFood:[
+       {name:"kimchi",fatty:false},
+       {name:"hamburger",fatty:true}
+    ]
+}
+
+console.log(nicoInfo.favFood[1].name);
